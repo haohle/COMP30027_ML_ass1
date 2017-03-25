cf9619ccb6cc04e1580afb28518c6628b3d80382
Hao Le <leh2@student.unimelb.edu.au>
2017-03-26 02:02:49 +1100

    implemented a very brute force approach to get_neighbours - needs testing and optimisation

--- ass1.py
+++ ass1.py
@@ -63,8 +63,23 @@ def get_neighbours(instance, training_data_set, k, method):
         list of (class, score) 2-tuples for each of the k best neighbours for
         the given instance
     '''
-    pass
+    # array approach - ignore for now
+    # neighbours = np.zeros(shape=[k, 2]) # creates an array of k by 2 can also use np.empty
+    # will need to convert neighbours back into a list before returning
+    # return neighbours.tolist()
 
+    # list approach
+    raw = []
+    for index, row in training_data_set.iterrows():
+        temp_class = row['Rings']
+        temp_score = compare_instance(instance, row, method)
+        # fill up the list of classes and scores
+        raw.append((temp_class, temp_score))
+
+    # will sort them from shortest to longest distance then return a list of up to k items
+    # the sorting part will probably take a while (not even sure what sorting algorithm is used)
+    # might be best to use a heap instead and continuously update the top k items
+    return sorted(raw)[:k]
 
 def predict_class(neighbours, method):
     '''
